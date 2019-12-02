ff1be4d697b0c1cd30e4bf609b514569c279f80f
fgw <fengguowang@17guagua.com>
2019-12-02 10:12:35 +0800

    init

---
+++ pages/shouye/shouye.js
@@ -0,0 +1,36 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    provinceInd: 0,
+    array: ['北京市', '天津市', '保定市'],
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  toNext: function () {
+    wx.navigateTo({
+      url: '../shouye/shouye'
+    })
+  }
+})
